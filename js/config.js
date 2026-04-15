625e3c755e0388868e0ce69d023e61e16a623060
produksitigermandiri-netizen <produksi.tigermandiri@gmail.com>
2026-04-15 14:43:02 +0700

    Delete js/config.js

--- js/config.js
+++
@@ -1,7 +0,0 @@
-// =============================================
-// GANTI dengan credentials Supabase kamu
-// =============================================
-const SUPABASE_URL = 'https://ssmwxtizalvubwmbvxxr.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzbXd4dGl6YWx2dWJ3bWJ2eHhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyMjYwMDUsImV4cCI6MjA5MTgwMjAwNX0.CZiN998WoV5ghRH2VXXDSAGs0o_sMj4zlWm8q9ID_TI'
-
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
